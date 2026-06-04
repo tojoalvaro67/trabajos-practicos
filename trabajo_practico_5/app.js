7effd2292c3a5dd42042406660b823978b3e157d
alumno <52178087@ivt.edu.ar>
2026-06-04 10:41:39 -0300

    4/6/20206

--- trabajo_practico_5/app.js
+++ trabajo_practico_5/app.js
@@ -1,6 +1,26 @@
 // declarar variable
 let numero1
+let numero2
+ let suma
+ let resta
+ let division
+ let multiplicacion
 //asignar valor
 numero1 = 10
+numero2 = 3
+suma = numero1 + numero2
+resta = numero1 - numero2
+division = numero1 / numero2
+multiplicacion = numero1 * numero2
 //declarar y asignar al mismo tiempo
-let numero2 =
+alert("El resultado de la suma es: " + suma)
+alert( "El resultado de la resta es: " + resta)
+console.log("El resultado de la multiplicacion es:" + multiplicacion)
+console.log( "El resultado de la division es:" + division)
+let nombre1
+let apellido1
+// asignar valor
+nombre1 = "alvaro"
+apellido1 = "tojo"
+//declarar y asignar vcalor al mismo tiempo
+alert( " El alumno se llama: " + nombre1+" " + apellido1)
